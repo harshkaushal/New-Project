b37c712f50145fefb80c6ea40fedc2fa0200b53a
ankesh1989 <ankesh1989@gmail.com>
2014-07-14 00:04:34 +0530

    Comited models

---
+++ Ekomsys/Ekomsys.Web/Models/NewsModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Ekomsys.Web.Models
+{
+    public  class NewsModel
+    {
+        public int News_Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public Nullable<System.DateTime> Posted_Date { get; set; }
+        public bool Is_Active { get; set; }
+        public Nullable<System.DateTime> Created_Date { get; set; }
+        public Nullable<int> Created_By { get; set; }
+        public Nullable<System.DateTime> Modify_Date { get; set; }
+        public Nullable<int> Modify_By { get; set; }
+    }
+}
