8bb2b755560a758814feba5e31a21aa4af880b54
navshbk <navs.hbk@gmail.com>
2014-07-14 23:44:53 +0530

    Usermanagement added

---
+++ Ekomsys/Ekomsys.Business/Classes/UserManagementBAL.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ekomsys.Entities;
+using Ekomsys.DataAccess.Classes;
+
+namespace Ekomsys.Business.Classes
+{
+    public class UserManagementBAL : Ekomsys.Business.Interfaces.IUserManagementBAL
+    {
+        UserManagementRepository _repository = null;
+        public List<tb_Users> GetAllUsers()
+        {
+            _repository = new UserManagementRepository();
+            return _repository.GetAllUsers();
+        }
+
+        public bool AddUser(tb_Users users)
+        {
+            _repository = new UserManagementRepository();
+
+            return _repository.AddUser(users);
+        }
+
+        public bool UpdateUser(tb_Users userModel)
+        {
+            _repository = new UserManagementRepository();
+            return _repository.UpdateUser(userModel);
+            
+        }
+
+        public bool DeleteUser(tb_Users user)
+        {
+            _repository = new UserManagementRepository();
+            return _repository.DeleteUser(user);
+        }
+    }
+}
---
+++ Ekomsys/Ekomsys.Business/Interfaces/IUserManagementBAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ekomsys.Entities;
+
+namespace Ekomsys.Business.Interfaces
+{
+   public interface IUserManagementBAL
+    {
+        bool AddUser(tb_Users news);
+        List<tb_Users> GetAllUsers();
+        bool UpdateUser(tb_Users userModel);
+        bool DeleteUser(tb_Users userModel);
+    }
+}
