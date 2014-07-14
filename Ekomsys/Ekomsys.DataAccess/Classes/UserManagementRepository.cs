e23fb3295764fd92bc97870d0b0cdab470cb977b
navshbk <navs.hbk@gmail.com>
2014-07-14 23:44:29 +0530

    usermanagement add

---
+++ Ekomsys/Ekomsys.DataAccess/Classes/UserManagementRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ekomsys.Entities;
+
+namespace Ekomsys.DataAccess.Classes
+{
+    public class UserManagementRepository : RepositoryBase<tb_Users>
+    {
+        public List<tb_Users> GetAllUsers()
+        {
+            using (DevSamplesEntities _dbEntity = new DevSamplesEntities())
+            {
+                List<tb_Users> dbList = _dbEntity.tb_Users.OrderByDescending(d => d.User_Id).ToList();
+                return dbList;
+            }
+        }
+
+        public bool AddUser(tb_Users userModel)
+        {
+            using (DevSamplesEntities _dbEntity = new DevSamplesEntities())
+            {
+                try
+                {
+                    _dbEntity.tb_Users.Add(userModel);
+                    _dbEntity.SaveChanges();
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+
+        public bool UpdateUser(tb_Users userModel)
+        {
+            using (DevSamplesEntities _dbEntity = new DevSamplesEntities())
+            {
+                try
+                {
+                    Update(userModel);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+        public bool DeleteUser(tb_Users userModel)
+        {
+            using (DevSamplesEntities _dbEntity = new DevSamplesEntities())
+            {
+                try
+                {
+                    Delete(userModel);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
--- Ekomsys/Ekomsys.DataAccess/Edmx/Ekomsys.Designer.cs
+++ Ekomsys/Ekomsys.DataAccess/Edmx/Ekomsys.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'F:\Ankesh Stuff\Projects\Sarbjit\Ekomsys\Ekomsys.DataAccess\Edmx\Ekomsys.edmx'. 
+﻿// Default code generation is disabled for model 'I:\Odesk Projects\Working code 14 july\Ekomsys\Ekomsys.DataAccess\Edmx\Ekomsys.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
